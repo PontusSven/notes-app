1ae1a44b70e01737e7646c266d16c8f15db471bc
Pontussven <pontus.svensson92@gmail.com>
2020-08-26 10:50:05 +0200

    implemented readNote

--- notes.js
+++ notes.js
@@ -6,15 +6,16 @@ const getNotes = () => 'Your notes...'
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
 
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
         })
         saveNotes(notes)
         console.log(chalk.green.inverse('New note added'))
+
     } else {
         console.log(chalk.red.inverse('Note title taken'))
     }
@@ -33,13 +34,32 @@ const removeNote = (title) => {
     saveNotes(notesToKeep)
 }
 
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.blue.inverse('Your notes:'))
+   
+    notes.forEach(note => {
+        console.log('Title: ' + note.title + ' Body: ' + note.body)
+    });
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+   if (note) {
+       console.log(chalk.green(note.title) + ' : ' + note.body)
+   } else {
+       console.log(chalk.red.inverse('Error no note found!'))
+   }
+}
+
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
 const loadNotes = () => {
-
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
@@ -54,5 +74,7 @@ const loadNotes = () => {
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
 }
